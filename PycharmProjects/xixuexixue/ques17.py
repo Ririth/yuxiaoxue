2a24efa78e395fc770f88783f61afa7100e8ca9c
yuxue <m13772189626@163.com>
2021-03-20 21:25:55 +0800

    剑指offer ques8提交

--- PycharmProjects/xixuexixue/ques17.py
+++ PycharmProjects/xixuexixue/ques17.py
@@ -13,12 +13,7 @@ class Solution:
         high = l - 1
         mid = int((low + high) / 2)
         while low != high:
-            if rotateArray[0] == rotateArray[mid]:
-                if rotateArray[0] > rotateArray[1]:
-                    return rotateArray[1]
-                else:
-                    return rotateArray[0]
-            elif rotateArray[0] < rotateArray[mid]:
+            if rotateArray[0] <= rotateArray[mid]:
                 low = mid + 1
                 mid = int((low + high) / 2)
             else:
@@ -26,6 +21,6 @@ class Solution:
                 low = 0
                 mid = int((low + high) / 2)
         return rotateArray[mid]
-    array1 = [3,4,5,6,1,2]
+    array1 = [3,4,5,1,2]
     self = ''
     print(minNumberInRotateArray(self,array1))
